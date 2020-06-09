06cb21e36371a3952e7d28b9e8f90cf5811fab3f
Yelyzaveta Rohovska <63359972+YelyzavetaRohovska@users.noreply.github.com>
2020-06-09 22:21:01 +0300

    Add files via upload

---
+++ preview/js/main.js
@@ -0,0 +1,40 @@
+/*jQuery(function(){
+	$('.header').height($(window).height());
+
+	
+});*/
+
+    document.querySelector('.header').style.height = document.documentElement.clientHeight + "px";
+
+
+
+
+
+// My jQuery 
+	// data-() .data() var h1 = $('h1').data('name');
+
+	//atributes $('h1[title!="test-word"]').css({'color':'red'});
+
+	//$('ul li:odd').css({'background-color':"red"});
+
+	//$('ul li:nth-child(3)').css({'background-color':'red'});
+
+	//:contains() , :empty, :parent, :has
+
+	// Events click dblclick mouseenter mouseleave 
+	//keypress keydown keyup change submit focus
+
+	//css hide() show() animate() fadeIn fadeOut
+
+	//resize scroll
+
+	//addClass removeClass toggleClass
+	
+	//text() html()
+
+	//append prepend after
+	//remove empty
+
+	//wrap unwrap !!!
+
+	
